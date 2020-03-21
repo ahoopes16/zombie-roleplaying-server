b71c1846868293252606c40dff5a815628abe6b8
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-03-21 01:48:20 -0500

    Guarantee database connection before starting the server
    Wrapping the server script in an async function allows
    us to await the database connection.
    
    This lets us crash fast if something goes wrong there
    and guarantee a db connection before letting users
    access the server.

--- src/server.ts
+++ src/server.ts
@@ -7,29 +7,31 @@ import logger from './util/logger.util'
 import DBConnector from './database'
 import env from './environment'
 
-const dbConnector = new DBConnector()
-dbConnector.connect()
+async function runServer(): Promise<void> {
+    await new DBConnector().connect()
 
-const app = new Koa()
-const router = new Router()
-const port = 8000
+    const app = new Koa()
+    const router = new Router()
     const port = env.port || 8000
 
-// Keep these as early as possible, takes care of parsing JSON, logging, and error handling
-app.use(bodyParser())
-app.use(KoaLogger(logger))
-app.use(errorHandler)
+    // Keep these as early as possible, takes care of parsing JSON, logging, and error handling
+    app.use(bodyParser())
+    app.use(KoaLogger(logger))
+    app.use(errorHandler)
 
-// TODO Extract these routes somewhere that makes more sense
-router.get('/error', async () => {
-    throw new Error('This is what will happen when there is a problem!')
-})
-router.get('/', async ctx => {
-    logger.info(`A route was called! It looked like ${ctx.path}!`)
-    ctx.body = 'This will be the zombie roleplaying game API!'
-})
+    // TODO Extract these routes somewhere that makes more sense
+    router.get('/error', async () => {
+        throw new Error('This is what will happen when there is a problem!')
+    })
+    router.get('/', async ctx => {
+        logger.info(`A route was called! It looked like ${ctx.path}!`)
+        ctx.body = 'This will be the zombie roleplaying game API!'
+    })
 
-app.use(router.routes())
-app.use(router.allowedMethods())
+    app.use(router.routes())
+    app.use(router.allowedMethods())
 
     app.listen(port, () => logger.info(`Zombie Roleplaying Server listening on port ${port}!`))
+}
+
+runServer()
