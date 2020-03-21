8ab9484642df2ca1bd043ce0027c82f806057c82
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-03-21 16:24:05 -0500

    Force the title of an encounter to be unique with Mongoose

--- src/models/encounter.model.ts
+++ src/models/encounter.model.ts
@@ -9,15 +9,17 @@ export interface Encounter extends Document {
     actions: string[]
     /** How many times this encounter has been run. Defaults to 0 */
     numberOfRuns: number
-    /** Created by Mongoose */
+    /** The date this document was created. Created by Mongoose */
     createdAt: Date
-    /** Created by Mongoose */
+    /** The last date this document was updated. Created by Mongoose */
     updatedAt: Date
+    /** The version number for this document. Created by Mongoose */
+    __v: number
 }
 
 const encounterSchema = new Schema(
     {
-        title: { type: String, required: true, trim: true },
+        title: { type: String, required: true, unique: true, trim: true },
         description: { type: String, required: true },
         actions: [String],
         numberOfRuns: { type: Number, default: 0 },
