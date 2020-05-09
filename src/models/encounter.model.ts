4e1d8b33729ab837316b5b82419a4b7adee9fd99
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-05-09 15:52:20 -0700

    Add examples and descriptions to Encounter fields

--- src/models/encounter.model.ts
+++ src/models/encounter.model.ts
@@ -1,19 +1,43 @@
 import { Schema, Document, model, Model } from 'mongoose'
 
-export interface Encounter extends Document {
-    /** A fun title for the encounter */
+/**
+ * Encounters are events that can happen to you as a player.
+ * Their titles must be unique, and a list of actions the player
+ * can take may be defined.
+ */
+export interface Encounter {
+    /**
+     * A fun title for the encounter.
+     * @example "A Zombie Grabs Your Leg!"
+     */
     title: string
-    /** Some text describing what happens in this encounter */
+
+    /**
+     * Some text describing what happens in this encounter.
+     * @example "You can tell from here that it has terrible breath."
+     */
     description: string
-    /** A list of actions the user must take when they hit this encounter */
-    actions: string[]
-    /** How many times this encounter has been run. Defaults to 0 */
+
+    /**
+     * A list of actions the user can take when they hit this encounter.
+     * @example ["Attempt to kick it away!"]
+     */
+    actions?: string[]
+
+    /**
+     * How many times this encounter has been run.
+     * Defaults to 0.
+     * @example 0
+     */
     numberOfRuns: number
-    /** The date this document was created. Created by Mongoose */
+
+    /** The date this document was created. Created by Mongoose. */
     createdAt: Date
-    /** The last date this document was updated. Created by Mongoose */
+
+    /** The last date this document was updated. Created by Mongoose. */
     updatedAt: Date
-    /** The version number for this document. Created by Mongoose */
+
+    /** The version number for this document. Created by Mongoose. */
     __v: number
 }
 
