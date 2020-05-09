094c34f2bffd98b21539541e52f159a08bdd9f12
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-05-09 15:57:12 -0700

    Rework error handler middleware for TSOA

--- src/middleware/error-handler.middleware.ts
+++ src/middleware/error-handler.middleware.ts
@@ -1,11 +1,30 @@
 import { Context, Next, Middleware } from 'koa'
+import { ValidateError } from 'tsoa'
+import logger from '../util/logger.util'
+
+export interface ValidateErrorJSON {
+    error: 'Validation Failed'
+    details: { [name: string]: unknown }
+}
 
 const errorHandler: Middleware = async (ctx: Context, next: Next) => {
     try {
         await next()
     } catch (error) {
-        ctx.status = error.statusCode || error.status || 500
-        ctx.body = { error: error.message }
+        if (error instanceof ValidateError) {
+            logger.error(`Caught validation error for ${ctx.request.path}:`, error.fields)
+            ctx.status = 400
+            const body: ValidateErrorJSON = {
+                error: 'Validation Failed',
+                details: error?.fields,
+            }
+            ctx.body = body
+
+            return
+        } else {
+            ctx.status = error.statusCode || error.status || 500
+            ctx.body = { error: error.message }
+        }
     }
 }
 
