6bc7dc870167a355d6e2d8fd14445f96de73c92e
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-05-12 15:47:59 -0700

    Pull status code from boom if it is a Boom error

--- src/middleware/error-handler.middleware.ts
+++ src/middleware/error-handler.middleware.ts
@@ -1,30 +1,17 @@
 import { Context, Next, Middleware } from 'koa'
-import { ValidateError } from 'tsoa'
-import logger from '../util/logger.util'
+import { isBoom } from '@hapi/boom'
 
 export interface ValidateErrorJSON {
-    error: 'Validation Failed'
-    details: { [name: string]: unknown }
+    error: string
 }
 
 const errorHandler: Middleware = async (ctx: Context, next: Next) => {
     try {
         await next()
     } catch (error) {
-        if (error instanceof ValidateError) {
-            logger.error(`Caught validation error for ${ctx.request.path}:`, error.fields)
-            ctx.status = 400
-            const body: ValidateErrorJSON = {
-                error: 'Validation Failed',
-                details: error?.fields,
-            }
-            ctx.body = body
-
-            return
-        } else {
-            ctx.status = error.statusCode || error.status || 500
-            ctx.body = { error: error.message }
-        }
+        ctx.status = isBoom(error) ? error.output.statusCode : error.statusCode || error.status || 500
+        const body: ValidateErrorJSON = { error: error.message }
+        ctx.body = body
     }
 }
 
