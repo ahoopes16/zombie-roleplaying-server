cba496ce259c11c3e2b3bb5ae06e83c81ccdf14e
Kevin Hoopes <kevin.alex.hoopes@gmail.com>
2020-05-14 19:43:28 -0700

    Create Initial Pass at a Weapon model (#10)
    * Create initial pass at a Weapon model
    
    Eventually I'll need to include a rarity, but I'm
    not sure how I want to implement that yet.
    
    * Convert numbers to bigints
    
    Bigints represent whole numbers, which better defines
    these properties

---
+++ src/models/weapon.model.ts
@@ -0,0 +1,69 @@
+import { Schema, Document, model, Model } from 'mongoose'
+
+/**
+ * Weapons are items that a player can use to help fend off the zombies.
+ * Attack power is defined as the number of die and the number of sides the die should have.
+ * Their titles must be unique.
+ */
+export interface Weapon {
+    /**
+     * The name of the weapon.
+     * @example "Baseball Bat"
+     */
+    name: string
+
+    /**
+     * Some text describing the weapon.
+     * @example "An old wooden bat that probably belonged to some kid."
+     */
+    description: string
+
+    /**
+     * The number of die to use when attacking with this weapon.
+     * @example 2
+     */
+    attackDieCount: bigint
+
+    /**
+     * The number of sides the die should have when attacking with this weapon.
+     * @example 10
+     */
+    attackDieSides: bigint
+
+    /**
+     * The number of times this weapon has been looted by players.
+     * @example 2
+     */
+    timesLooted: bigint
+
+    /**
+     * The date this document was created. Created by Mongoose.
+     * @example "2020-01-01T00:00:00.000Z"
+     */
+    createdAt: Date
+
+    /**
+     * The last date this document was updated. Created by Mongoose.
+     * @example "2020-01-01T00:00:00.000Z"
+     */
+    updatedAt: Date
+
+    /**
+     * The version number for this document. Created by Mongoose.
+     * @example 1
+     */
+    __v: bigint
+}
+
+const weaponSchema = new Schema(
+    {
+        name: { type: String, required: true, unique: true, trim: true },
+        description: { type: String, required: true },
+        attackDieCount: { type: Number, required: true },
+        attackDieSides: { type: Number, required: true },
+        timesLooted: { type: Number, required: true },
+    },
+    { timestamps: true },
+)
+
+export default (): Model<Weapon & Document> => model<Weapon & Document>('Weapon', weaponSchema)
